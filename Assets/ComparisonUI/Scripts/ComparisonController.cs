c5b856265453a24ef197ee4768b12eafd3d2a718
Zachary Helm <zac1790@gmail.com>
2023-08-06 12:17:54 -0400

    Basic comparison setup with FPS tracking 4 times/sec. Pressing A and B switches between comparisons. Test comparison shows same results, in line with the concept that vertex count alone has negligible effect on performance.

--- Assets/ComparisonUI/Scripts/ComparisonController.cs
+++ Assets/ComparisonUI/Scripts/ComparisonController.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class ComparisonController : MonoBehaviour
@@ -9,13 +8,17 @@ public class ComparisonController : MonoBehaviour
     private int currentQuantity = 0;
     private GameObject[] activeInstances;
     public float zOffset;
-    public int rows = 1;
-    public float rowOffset;
+    private float currentZ;
+    public int rows = 5;
+    public float rowOffset = 1;
     [SerializeField]
     private ObjectListScriptableObject objectListHolder;
     private GameObject[] prefabList;
     private bool leftActive = false;
     private bool rightActive = false;
+    private GameObject prefabA;
+    private GameObject prefabB;
+    private GameObject placingObject;
 
     [SerializeField]
     private TMP_Dropdown SelectADropdown;
@@ -23,10 +26,28 @@ public class ComparisonController : MonoBehaviour
     private TMP_Dropdown SelectBDropdown;
     [SerializeField]
     private GameObject EditPanel;
+    [SerializeField]
+    private TMP_Text quantityText;
+    [SerializeField]
+    private TMP_Text unityVersionText;
+    [SerializeField]
+    private TMP_Text fpsTextA;
+    [SerializeField]
+    private TMP_Text fpsTextB;
+
+    // FPS tracking
+    private int frameCount = 0;
+    private float deltaTime = 0.0f;
+    private float fps = 0.0f;
+    public float updateFPSRate = 4.0f; // Updates per sec.
+
 
     void Start()
     {
         currentQuantity = 0;
+        currentZ = 0;
+
+        unityVersionText.text = "Unity " + Application.unityVersion;
 
         prefabList = objectListHolder.prefabsToCompare;
         List<string> optionsList = new List<string>();
@@ -38,32 +59,72 @@ public class ComparisonController : MonoBehaviour
         SelectADropdown.AddOptions(optionsList);
         SelectBDropdown.ClearOptions();
         SelectBDropdown.AddOptions(optionsList);
+        if (prefabList.Length > 0)
+        {
+            prefabA = prefabList[0];
+            prefabB = prefabList[0];
+        }
+        else
+        {
+            Debug.LogError("No prefabs in list found on scriptable object. Please create and attach an ObjectListScriptableObject and list prefabs for comparison in it.");
+        }
+
     }
 
     void Update()
     {
-        
+        UpdateFPS();
+        // Continue adding rows if below target quantity 
+        if(currentQuantity < activeInstances.Length)
+        {
+            PlaceRow();
+        }
+    }
+
+    private void UpdateFPS()
+    {
+        frameCount++;
+        deltaTime += Time.deltaTime;
+        if(deltaTime > 1.0f / updateFPSRate)
+        {
+            fps = frameCount / deltaTime;
+            frameCount = 0;
+            deltaTime -= 1.0f / updateFPSRate;
+            // update text
+            fpsTextA.text = prefabA.name + "<br>FPS: " + Mathf.RoundToInt(fps);
+            fpsTextB.text = prefabB.name + "<br>FPS: " + Mathf.RoundToInt(fps);
+        }
     }
 
 
     public void PressASide()
     {
-
+        StartPlacing(prefabA);
+        leftActive = true;
+        rightActive = false;
+        fpsTextA.gameObject.SetActive(true);
+        fpsTextB.gameObject.SetActive(false);
     }
 
     public void PressBSide()
     {
-
+        StartPlacing(prefabB);
+        leftActive = false;
+        rightActive = true;
+        fpsTextA.gameObject.SetActive(false);
+        fpsTextB.gameObject.SetActive(true);
     }
 
     public void SelectObjectA(int val)
     {
         Debug.Log(val);
+        prefabA = prefabList[val];
     }
 
     public void SelectObjectB(int val)
     {
         Debug.Log(val);
+        prefabB = prefabList[val];
     }
 
     public void DoneEditing()
@@ -79,5 +140,42 @@ public class ComparisonController : MonoBehaviour
     public void UpdateQuantity(string quantStr)
     {
         instanceQuantity = Mathf.RoundToInt(Mathf.Max(1, int.Parse(quantStr)));
+    }
+
+    private void StartPlacing(GameObject placeMe)
+    {
+        ClearObjects();
+        placingObject = placeMe;
+        activeInstances = new GameObject[instanceQuantity];
+    }
+
+    private void ClearObjects()
+    {
+        for(int i=0; i<currentQuantity; i++)
+        {
+            Destroy(activeInstances[i]);
+        }
+
+        currentQuantity = 0;
+        currentZ = 0;
+    }
+
+    private void PlaceRow()
+    {
+        float placementWidth = rowOffset * (rows - 1);
+        float xOffset = -placementWidth / 2;
+
+        int curRowCount = Mathf.RoundToInt(Mathf.Min(activeInstances.Length - currentQuantity, rows));
+
+        for (int i = 0; i < curRowCount; i++)
+        {
+            activeInstances[currentQuantity] = Instantiate(placingObject);
+            activeInstances[currentQuantity].transform.Translate(new Vector3(xOffset, 0, currentZ));
+            currentQuantity += 1;
+            xOffset += rowOffset;
+        }
+
+        quantityText.text = currentQuantity + "/" + activeInstances.Length;
+        currentZ += zOffset;
     }
 }
